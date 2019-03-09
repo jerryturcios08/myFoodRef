4b39d17fa411fd37ffcc028f1dd658b78b27eb54
Jerry Turcios <jerryturcios08@gmail.com>
2019-03-09 15:02:55 -0500

    Refactor routes

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 
-import Home from "./views/Home.vue";
+// import Home from "./views/Home.vue";
 
 Vue.use(Router);
 
@@ -9,10 +9,10 @@ export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    }
+    // {
+    //   path: "/",
+    //   name: "home",
+    //   component: Home
+    // }
   ]
 });
